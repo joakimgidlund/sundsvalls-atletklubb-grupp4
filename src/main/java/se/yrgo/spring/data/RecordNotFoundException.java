d0db8fcb69272807304b29c298718cbc83c7c9ba
joakimgidlund <jockegidlund@gmail.com>
2025-05-22 15:30:35 +0200

    created exception for data access

---
+++ src/main/java/se/yrgo/spring/data/RecordNotFoundException.java
@@ -0,0 +1,10 @@
+package se.yrgo.spring.data;
+
+public class RecordNotFoundException extends Exception {
+
+	/**
+	 * Just to stop warnings.
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
