23641d0318916bac8575f1b746d4cf1735695191
joakimgidlund <jockegidlund@gmail.com>
2025-05-25 16:53:39 +0200

    resolution change

--- src/main/java/se/yrgo/spring/client/Client.java
+++ src/main/java/se/yrgo/spring/client/Client.java
@@ -6,9 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import se.yrgo.spring.domain.GymClass;
-import se.yrgo.spring.services.CustomerService;
-import se.yrgo.spring.services.GymClassService;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -18,14 +15,11 @@ public class Client extends Application {
     @Override
     public void start(Stage stage) {
 
-        Scene scene = new Scene(new StackPane(), 360, 480);
-        // add an event handler to the label control
+        Scene scene = new Scene(new StackPane(), 520, 480);
         try {
-            // here we load the XML file and makes it into a node
             FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("/fxml/main.fxml"));
             Parent root = fxmlLoader.load();
 
-            // then we set the root node to this new loaded node
             scene.setRoot(root);
         } catch (IOException ex) {
             throw new UncheckedIOException(ex);
