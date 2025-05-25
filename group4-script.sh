a03fc014d09b47eb84adf629706d04733eb18006
PeterHjelm <peter.hjelm1@gmail.com>
2025-05-25 13:35:10 +0200

    creating a script to run the code with

---
+++ group4-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "==== Sundsvalls Atletklubb - Instructions ==="
+echo " "
+echo "1. Download the project from GitHub: https://github.com/joakimgidlund/sundsvalls-atletklubb-grupp4"
+echo "2. Extract the zip-folder"
+echo "3. Navigate to the project root, the folder named "src" "
+echo "4. If you want to run this script and the project you need to have Maven installed"
+echo " "
+echo "5. Run this script by typing ./group4-script.sh"
+echo "Building the project with Maven..."
+mvn clean compile
+
+echo " "
+echo "Starting the client class..." 
+mvn exec:java -Dexec.mainClass="se.yrgo.spring.client.Client"
