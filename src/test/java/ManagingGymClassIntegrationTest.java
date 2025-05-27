3619c14a2935bdc929fd9774f7424ddaead98510
joakimgidlund <jockegidlund@gmail.com>
2025-05-27 18:52:30 +0200

    add a couple tests and comments

--- src/test/java/ManagingGymClassIntegrationTest.java
+++ src/test/java/ManagingGymClassIntegrationTest.java
@@ -16,10 +16,15 @@ import se.yrgo.spring.services.CustomerService;
 import se.yrgo.spring.services.GymClassNotFoundException;
 import se.yrgo.spring.services.GymClassService;
 
+/**
+ * Test class for GymClassDao implementation.
+ * 
+ * @author joakimgidlund
+ */
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration({ "/other-tiers.xml", "/datasource-test.xml" })
 @Transactional
-public class ManagingGymClassIntegrationTest {
+class ManagingGymClassIntegrationTest {
 
     @Autowired
     private GymClassService classes;
@@ -27,8 +32,12 @@ public class ManagingGymClassIntegrationTest {
     @Autowired
     private CustomerService customers;
 
+    /**
+     * Tests adding new classes to the database by registering them
+     * and then getting all classes from the database and matching the count.
+     */
     @Test
-    public void testAddingClass() {
+    void testAddingClass() {
         classes.registerNewClass(new GymClass("123", "test1", 123));
         classes.registerNewClass(new GymClass("123", "test2", 123));
 
@@ -36,15 +45,55 @@ public class ManagingGymClassIntegrationTest {
         assertEquals(2, tableLength, "There should be two registered classes in the database.");
     }
 
+    /**
+     * Tests finding a class with a bad id. Makes sure we throw
+     * GymClassNotFoundException on errors.
+     */
     @Test
-    public void testFindWrongClass() {
+    void testFindWrongClassById() {
         assertThrows(GymClassNotFoundException.class, () -> {
-            GymClass found = classes.getGymClassById("bad id");
+            classes.getGymClassById("bad id");
         });
     }
 
+    /**
+     * Tests searching for a name that doesn't exist in the database.
+     * Should throw GymClassNotFoundException.
+     */
     @Test
-    public void testClassCustomerJoin() throws GymClassNotFoundException, CustomerNotFoundException {
+    void testFindWrongByName() {
+        assertThrows(GymClassNotFoundException.class, () -> {
+            classes.getGymClassByName("bad name");
+        });
+    }
+
+    /**
+     * Tests deletion of gym classes from the database.
+     * 
+     * @throws GymClassNotFoundException
+     */
+    @Test
+    void testDelete() throws GymClassNotFoundException {
+        GymClass gClass = new GymClass("001", "Test class", 130);
+        classes.registerNewClass(gClass);
+
+        assertEquals(1, classes.getAllGymClasses().size(), "Checks that the class was added.");
+
+        classes.deleteClassFromCatalogue(gClass);
+        assertEquals(0, classes.getAllGymClasses().size(), "The table should be empty after deletion.");
+    }
+
+    /**
+     * Tests adding customers to a gym class. Registers a customer to a gymclass and then 
+     * a gymclass to that customer, then we save them to the database. Finally, we find
+     * the added gymclass and checks how many customers are attending. Similarily we get all 
+     * classes of the added customer in the database.
+     * 
+     * @throws GymClassNotFoundException
+     * @throws CustomerNotFoundException
+     */
+    @Test
+    void testClassCustomerJoin() throws GymClassNotFoundException, CustomerNotFoundException {
         Customer customer = new Customer("1", "Joakim", "joakim@mail.com");
         GymClass gClass = new GymClass("123-123", "Power", 100);
 
@@ -54,8 +103,8 @@ public class ManagingGymClassIntegrationTest {
 
         classes.registerNewClass(gClass);
 
-        
-        int classCustomers = classes.getAllCustomers(gClass.getClassId()).size();
+        GymClass foundClass = classes.getGymClassById(gClass.getClassId());
+        int classCustomers = foundClass.getAttendees().size();
         int customerClasses = customers.getAllCustomerClasses(customer.getCustomerId()).size();
         assertEquals(1, customerClasses, "One class added to customer's classlist");
         assertEquals(1, classCustomers, "One customer added to class attendancelist.");
